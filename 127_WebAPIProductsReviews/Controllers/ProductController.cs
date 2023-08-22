b95fadf49f073dd52a0544ad41f4256113b8e73f
Nikita Obergan <Obergannikita@gmail.com>
2023-08-22 16:45:02 -0400

    Add Post, Put, Delete in Product Controller

---
+++ 127_WebAPIProductsReviews/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using _127_WebAPIProductsReviews.Data;
+using _127_WebAPIProductsReviews.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _127_WebAPIProductsReviews.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Product> products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest();
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest();
+
+            Product existingProduct = _context.Products.FirstOrDefault(f => f.Id == id);
+
+            if (existingProduct is null)
+                return NotFound();
+
+            existingProduct.Name = product.Name;
+            existingProduct.Price = existingProduct.Price;
+
+            _context.SaveChanges();
+            return Ok(existingProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Product product = _context.Products.FirstOrDefault(f => f.Id == id);
+
+            if (product is null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
+    }
+}

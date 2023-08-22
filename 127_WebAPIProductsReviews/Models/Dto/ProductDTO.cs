4bc71964f6e9fb176392a2dda3c1fbff22d2b24b
Nikita Obergan <Obergannikita@gmail.com>
2023-08-22 12:41:37 -0400

    Add DTOs

---
+++ 127_WebAPIProductsReviews/Models/Dto/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace _127_WebAPIProductsReviews.Models.Dto
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double AverageRating { get; set; }
+
+        // Nav props
+        public ICollection<Review> Reviews { get; set; }
+    }
+}

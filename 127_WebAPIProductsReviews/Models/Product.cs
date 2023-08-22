d469160a87693937d5414863a917bd8a90966b24
Nikita Obergan <Obergannikita@gmail.com>
2023-08-22 12:38:27 -0400

    Add Models, Product and Reviews

---
+++ 127_WebAPIProductsReviews/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace _127_WebAPIProductsReviews.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // Nav props
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ 127_WebAPIProductsReviews/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace _127_WebAPIProductsReviews.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+
+        // Nav props
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+    }
+}

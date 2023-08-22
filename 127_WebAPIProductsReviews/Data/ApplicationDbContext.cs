16154168220b700d4712dbe366b183fb9b2c6ad2
Nikita Obergan <Obergannikita@gmail.com>
2023-08-22 12:32:30 -0400

    Add connectin string, Packages, ApplicationDbContext Class

---
+++ 127_WebAPIProductsReviews/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _127_WebAPIProductsReviews.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}

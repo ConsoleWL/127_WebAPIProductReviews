b909768b0857573ee5ea0be0ca895e4c14110575
Nikita Obergan <Obergannikita@gmail.com>
2023-08-22 12:53:40 -0400

    add migrations

---
+++ 127_WebAPIProductsReviews/Controllers/ReviewController.cs
@@ -0,0 +1,19 @@
+﻿using _127_WebAPIProductsReviews.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _127_WebAPIProductsReviews.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+    }
+}
